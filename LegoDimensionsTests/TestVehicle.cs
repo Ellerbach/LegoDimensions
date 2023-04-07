f7d6ef7afac53b6d1acd52a3f1ed092b70f9856d
Laurent Ellerbach <laurelle@microsoft.com>
2023-04-07 16:49:27 +0200

    Adjusting test to force rebuild and republish

--- LegoDimensionsTests/TestVehicle.cs
+++ LegoDimensionsTests/TestVehicle.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to Laurent Ellerbach and contributors under one or more agreements.
 // Laurent Ellerbach and contributors license this file to you under the MIT license.
 
-using LegoDimensions.Tag;
-
 namespace LegoDimensionsTests
 {
     public class TestVehicle
